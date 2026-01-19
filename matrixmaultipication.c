08d072611647ddbefb3cde1f41d9e565ded5a01b
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-01-19 17:28:36 +0530

    Add files via upload

---
+++ matrixmaultipication.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int i,j,a[3][3],b[3][3],c[3][3];
+printf("Enter first matrix");
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        scanf("%d",&a[i][j]);
+    }
+}
+printf("Enter second matrix");
+for(i=0;i<3;i++)
+{
+    for(j=0;j<3;j++)
+    {
+        scanf("%d",&b[i][j]);
+    }
+}
+
+
+}

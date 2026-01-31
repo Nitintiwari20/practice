e1a7f1b23bea5350f8578b620a7483de4bba8f6b
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-01-31 19:01:22 +0530

    Add files via upload

---
+++ tutorial13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,index=0;
+    printf("enter a number");
+    scanf("%d",&num);
+
+    do
+    {
+        printf("%d",index+1);
+        index=index+1;
+    } while (index<num);
+    
+    return 0;
+
+}

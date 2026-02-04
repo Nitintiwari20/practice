8e55b76fe1b971dbf6b0946002dcbdf3cba5ca5b
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-02-04 21:03:11 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,a[4];
+    
+    cout << "Enter your array:";
+    for(i=0;i<4;i++){
+        cin>> i[a];
+    }
+    return 0;
+}

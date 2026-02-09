53197c5cdab4fca347ba2c6808b1f038fa00d27f
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-02-09 20:42:23 +0530

    Add files via upload

---
+++ nestedloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i , j;
+    int n = 3;
+
+    for(i=1;i<=n;i++){//line
+
+    int m = 4;
+    for(j=1; j<=m; j++){
+        cout << "*";
+    }
+    cout << endl;
+    }
+    }

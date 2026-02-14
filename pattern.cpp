a7503b156f5dcf29be1b51471cd5b3b256939713
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-02-14 19:14:00 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n=3,num=1;
+    for(i=0;i<n;i++){
+
+        for(j=0;j<n;j++){
+            cout <<num;
+            num++;
+        }
+    cout<<endl;
+    }
+
+       return 0; 
+}
+
+
+
+
+//     }
+//   int i,j,n=4;
+//     for(i=0;i<n;i++){
+
+//         char ch = 'A';
+//         for(j=0;j<n;j++){
+//             cout << ch;
+//             ch = ch + 1;
+//         }
+//     cout<<endl;
+//     }
+
+//        return 0; 
+//     }

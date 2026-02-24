2b9a775c2dd2d639a6d22607b41ced20901ac7e7
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-02-24 19:27:40 +0530

    Add files via upload

---
+++ 01Nested.py
@@ -0,0 +1,13 @@
+num=int(input("enter a number:"))
+print("your value is:",num)
+if(0>num):
+    print("negative")
+elif(0<num):
+    if(num<=10):
+        print("number is 0 to 10")
+    elif(num<=20):
+        print("Number is 10 to 20")
+    else:
+        print("number is grater them 20")
+else:
+    print("number is zero")

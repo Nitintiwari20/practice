90066e5641467feb9f00e4ae4429abf711d38d60
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-03-22 21:12:38 +0530

    Add files via upload

---
+++ array1.py
@@ -0,0 +1,10 @@
+# find common elements in sorted arrays
+arr1 = [1,2,3,4]
+arr2 = [4,5,2,6]
+arr3 = [7,4,2,8]
+
+for i in arr1:
+    for j in arr2:
+        for k in arr3:
+            if i==j==k:
+                print(i)

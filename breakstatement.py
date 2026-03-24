6c3cc2d82e4a9c06de2351f703ba3bcf9f4d8835
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-03-24 09:24:31 +0530

    Add files via upload

---
+++ breakstatement.py
@@ -0,0 +1,11 @@
+# for i in range(12):
+#     if(i==10):
+#         break
+#     print("5 x",i+1,"=",5*(i+1))
+# print("loop ko chodker nikal gya")
+
+for i in range(12):
+    if(i==10):
+        print("skip the iteration")
+        continue
+    print("5 x",i,"=",5*1)

2fe1d10b87ed8a853ae1aec48cd13f82e23a3e80
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-04-01 21:05:12 +0530

    Add files via upload

---
+++ ExceptionHandling.py
@@ -0,0 +1,11 @@
+a = input("Enter the number:")
+print(f"Multiplication table of {a} is:")
+try:
+    for i in range(1,11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+
+except Exception as e:
+    print("sorry some error occurred")
+
+print("some imp lines of code")
+print("End of program")

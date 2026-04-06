328804cc36e19131b4bb2b06ef9cbe9d6085d0f3
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-04-06 21:50:04 +0530

    Add files via upload

---
+++ Function12.py
@@ -0,0 +1,19 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("second number is greater or equal")
+
+def islesser(a,b):
+    pass
+a=9
+b=8
+isGreater(a, b)
+calculateGmean(a,b)
+c=7
+d=9
+isGreater(c, d)
+calculateGmean(c,d)

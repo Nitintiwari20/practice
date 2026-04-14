69cba515b175027a567452fefdd09dc76a1ac7c8
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-04-14 21:41:11 +0530

    Add files via upload

---
+++ list-02.py
@@ -0,0 +1,10 @@
+list = []
+a = (input("Enter you favorite movie name:"))
+list.append(a)
+b = (input("Enter you second favorite movie name:"))
+list.append(b)
+c = (input("Enter you third favorite movie name:"))
+list.append(c)
+
+print(list)
+ 

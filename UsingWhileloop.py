4875b0d5d3a00c01cfd8fa8109d6e588cd634634
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-05-01 12:11:47 +0530

    Add files via upload

---
+++ UsingWhileloop.py
@@ -0,0 +1,14 @@
+# i=int(input("Enter the number:"))
+# while(i<34):
+#     i=int(input("Enter the number:"))
+#     print(i)
+
+x=5
+while(x>0):
+    print(x)
+    x=x-1
+else:
+    print("I am inside else")
+
+
+#do while loop is important for interviews

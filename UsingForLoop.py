64aad357ce737b8ad46dbaeb8ea0297f1bb5026f
Nitin Tiwari <20nitin9tiwari2005@gmail.com>
2026-05-02 19:56:50 +0530

    Add files via upload

---
+++ UsingForLoop.py
@@ -0,0 +1,14 @@
+# name ='Nitin'
+# for i in name:
+#     print(i)
+#     if(i=="n"):
+#         print("NitinTiwari")
+
+# colors = ["red","green","blue","yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+x=int(input("Enter a value:"))
+for k in range(x,x*10+1,x):
+    print(k)
